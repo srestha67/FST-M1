952bc8a5ccc8723d1e8f081731a21be0c1163585
srestha67 <132567960+srestha67@users.noreply.github.com>
2023-11-30 20:28:30 +0530

    Add files via upload
    Appium Project

---
+++ Appium/Project/Activity1.java
@@ -0,0 +1,31 @@
+package Project;
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Activity1 {
+    AndroidDriver driver;
+
+    // Set up method
+    @BeforeClass
+    public void setUp() throws MalformedURLException {
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setPlatformName("android");
+        options.setAutomationName("UIAutomator2");
+        options.setAppPackage("com.oneplus.calculator");
+        options.setAppActivity(".Calculator");
+        options.noReset();
+
+        // Server Address
+        URL serverURL = new URL("http://localhost:4723/wd/hub");
+
+        // Driver Initialization
+        driver = new AndroidDriver(serverURL, options);
+    }
+}
